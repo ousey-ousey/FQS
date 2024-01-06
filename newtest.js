0adf7d79d166918c18139248c1bc13346c2a56a5
UoOsSe <143217955+ousey-ousey@users.noreply.github.com>
2024-01-06 06:58:44 +0200

    new

---
+++ newtest.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let minus = true;
+
+    function toggleContent(i) {
+        let t = document.getElementById("p-" + i);
+        let p = document.getElementById("pt-" + i);
+
+        if (minus) {
+            t.style.setProperty('--before-content', 'url("/assets/images/icon-minus.svg")');
+            minus = false;
+            p.style.display = "block";
+        } else {
+            t.style.setProperty('--before-content', 'url("/assets/images/icon-plus.svg")');
+            minus = true;
+            p.style.display = "none";
+        }
+    }
+
+    for (let i = 1; i < 5; i++) {
+        let t = document.getElementById("p-" + i);
+        t.addEventListener('click', () => {
+            toggleContent(i);
+        });
+    }
+});
